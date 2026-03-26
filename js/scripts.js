2e04876053aa5bfe9b287c524ccbe9ef6fd98d56
262022-collab <262022@virtualwindow.co.za>
2026-03-26 13:21:24 +0200

    Added tooltips to the social media icons above footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,2 @@
-console.log("JS is working");
-const images = ["assets/img/imageSlider1.png", "assets/img/imageSlider1.png"];
-let index = 0;
 
-const slider = document.getElementById("sliderimgone");
 
-setInterval(() => {
-    index++;
-    if (index >= images.length) {
-        index = 0;
-    }
-    slider.src = images[index];
-}, 2000);
