4ba1f449f419d3749a6d0f16ebc4634ac5e8f9a0
262022-collab <262022@virtualwindow.co.za>
2026-05-29 22:11:14 +0200

    Counters are 100% working.
    Dont need to touch them for a while

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,25 @@ let plusBut2=document.getElementById("plusBut2");
 let minBut2=document.getElementById("minBut2");
 let countDisp2=document.getElementById("countDisp2");
 
+let plusBut3=document.getElementById("plusBut3");
+let minBut3=document.getElementById("minBut3");
+let countDisp3=document.getElementById("countDisp3");
 
-let count1=0;
+let plusBut4=document.getElementById("plusBut4");
+let minBut4=document.getElementById("minBut4");
+let countDisp4=document.getElementById("countDisp4");
+
+let plusBut5=document.getElementById("plusBut5");
+let minBut5=document.getElementById("minBut5");
+let countDisp5=document.getElementById("countDisp5");
+
+let plusBut6=document.getElementById("plusBut6");
+let minBut6=document.getElementById("minBut6");
+let countDisp6=document.getElementById("countDisp6");
+
+
+let [count1,count2,count3,count4,count5,count6]=[0,0,0,0,0,0];
+
 function incCount1(){
     for(let i=0;i<1;i++){
         count1++;}
@@ -19,7 +36,7 @@ function decCount1(){
             count1--; } }
     countDisp1.textContent=count1;}
 
-let count2=0;
+
 function incCount2(){
     for(let i=0;i<1;i++){
         count2++;}
@@ -31,14 +48,50 @@ function decCount2(){
             count2--;}}
     countDisp2.textContent=count2;}
 
+    
+function incCount3(){
+    for(let i=0;i<1;i++){
+        count3++;}
+    countDisp3.textContent=count3;}
 
+function decCount3(){
+    for(let i=0;i<1;i++){
+        if(count3>0){
+            count3--;}}
+    countDisp3.textContent=count3;}
 
+    function incCount4(){
+    for(let i=0;i<1;i++){
+        count4++;}
+    countDisp4.textContent=count4;}
 
+function decCount4(){
+    for(let i=0;i<1;i++){
+        if(count4>0){
+            count4--;}}
+    countDisp4.textContent=count4;}
 
+     function incCount5(){
+    for(let i=0;i<1;i++){
+        count5++;}
+    countDisp5.textContent=count5;}
 
+function decCount5(){
+    for(let i=0;i<1;i++){
+        if(count5>0){
+            count5--;}}
+    countDisp5.textContent=count5;}
 
+     function incCount6(){
+    for(let i=0;i<1;i++){
+        count6++;}
+    countDisp6.textContent=count6;}
 
-
+function decCount6(){
+    for(let i=0;i<1;i++){
+        if(count6>0){
+            count6--;}}
+    countDisp6.textContent=count6;}
 
 
 plusBut1.addEventListener("click", incCount1);
@@ -46,3 +99,15 @@ minBut1.addEventListener("click", decCount1);
 
 plusBut2.addEventListener("click", incCount2);
 minBut2.addEventListener("click", decCount2);
+
+plusBut3.addEventListener("click", incCount3);
+minBut3.addEventListener("click", decCount3);
+
+plusBut4.addEventListener("click", incCount4);
+minBut4.addEventListener("click", decCount4);
+
+plusBut5.addEventListener("click", incCount5);
+minBut5.addEventListener("click", decCount5);
+
+plusBut6.addEventListener("click", incCount6);
+minBut6.addEventListener("click", decCount6);
