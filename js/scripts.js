b3921b5e76bd96c2a15816d072b6016c2ea11e30
262022-collab <262022@virtualwindow.co.za>
2026-05-29 20:25:43 +0200

    working js counter. BUT MAJOR ISSUE
    Counter is only working for blue dragon. used multiple duplicate ids, so getelementbyid only grabs the first

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,26 @@
+let plusButB=document.getElementById("plusButB");
+let minButB=document.getElementById("minButB");
+let countDisp=document.getElementById("countDisp");
 
+let count=0;
+
+plusButB.addEventListener("click", incCount);
+
+function incCount(){
+    for(let i=0;i<1;i++){
+        count++;
+    }
+    countDisp.textContent=count;
+}
+
+minButB.addEventListener("click", decCount);
+
+function decCount(){
+    for(let i=0;i<1;i++){
+        if(count>0){
+            count--;
+        }
+    }
+    countDisp.textContent=count;
+}
 
